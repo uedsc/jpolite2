a92baf1ecfa3f853dee6243514ee632bdc2521e3
bwstudios <bwstudios@52608ca8-52a9-11de-936b-e9512c4d2277>
2009-06-08 20:12:32 +0000

    jQuery extensions added to support extendable widgetization mechanism Preliminary messaging handling framework

--- jpolite2/js/jpolite.core.js
+++ jpolite2/js/jpolite.core.js
@@ -3,6 +3,80 @@ function jPolite() {
 	this._Footer = $("#footer");
 	this._Tabs = $("#header_tabs");
 	this._Main = $("#main");
+	this._Containers = $(".main_containers");
+	this._Modules = $(".module");
+
+	function changeLayout(){
+		//Apply on _Main and/or _Containers
+	};
+
+	this.handleMessage = function(m) {
+		//Process message from server side
+	};
 };
 
+/*
+ * jQuery extensions to support widgetization actions on newly DOM node
+ * Applicable to module_content, helper, dynamic generated stuff ...
+ */
+$.extend({
+	LiveControl:{},
+	AdvControls:{},
+	addLiveControl: function(selector, event, handler) {
+		$(selector).live(event, handler);
+		$.LiveControls[selector] = {e:event, h:handler};
+	},
+	addAdvControls: function(options) {	//{selector:handler}
+		$.extend($.AdvControls, options);
+	}
+});
 
+$.fn.extend({
+	widgetize: function() {
+		return this.each(function() {
+			x = this;
+			$.each($.AdvControls, function(selector,handler){
+				handler.apply($(selector, x));
+			})
+		});
+	}
+});
+
+/*
+ * XML Data Object, cache+view layer for server side resources
+ * with Embedded RESTful resource discovery mechanism
+ * Currently only support JSON formatted data
+ */
+function XDO() {
+	this.XDOS = {};
+	this.UIOS = {};
+
+	this.handleMessage = function(m) {
+		//Process message from server side
+	};
+};
+
+/*
+ * Document Object and surpporting functions for Event Processing and beyond
+ * Custom Event Registration & Hooking also Supported
+ */
+function DOC(){
+	this._doc = $(document);
+	
+	function MessageHandler(json){
+		switch (json.target) {
+			case 'module': //update content of a moduke
+				break;
+			case 'jpolite': //find out what the target: header, tab#id, helper, container#id, module#id
+				break;
+			case 'resource': //find out which XDO to handle, name#url
+				break;
+			case 'message': //show some alerts to user
+				break;
+			case 'errors': //Prompt user about errors
+				break;
+		};
+	};
+};
+	
+$(function(){});
